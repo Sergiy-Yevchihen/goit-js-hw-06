c72942704bd7a313aaae6c9fee3a2018cc32656c
Serhii Yevchishen <magodza06@gmail.com>
2023-02-18 13:53:44 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+
+
+const navEl = document.querySelector('#ingredients');
+
+
+const elements = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+ 
+ const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredients[i];
+  elements.push(itemEl);
+   
+}
+
+navEl.append(...elements);
+console.log(navEl);
