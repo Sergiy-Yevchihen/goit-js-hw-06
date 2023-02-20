e40405f0fa818a7b3dfab04ee10fa0216c45e5b2
Serhii Yevchishen <magodza06@gmail.com>
2023-02-20 22:00:54 +0200

    add-js-task-7

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById('font-size-control');
+const textEl = document.getElementById('text');
+
+
+const fontSizeControl = ({ currentTarget }) =>
+  (textEl.style.fontSize = `${currentTarget.value}px`);
+
+inputEl.addEventListener('input', fontSizeControl);
