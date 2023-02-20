ed81973756abf02a848d92f1480298483e55ab19
Serhii Yevchishen <magodza06@gmail.com>
2023-02-20 15:34:51 +0200

    add- task-6-js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const inputEl = document.querySelector("#name-input");
 console.log(inputEl);
 const spanEl = document.querySelector("#name-output");
 //  
-
 const onSpanElSabmit = ({ currentTarget }) => {
     if (currentTarget.value.trim() !== '') {
        spanEl.textContent = currentTarget.value.trim(); 
