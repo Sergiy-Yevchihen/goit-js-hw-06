9812881ecf706ce8137bc1b0eae318851fd72639
Serhii Yevchishen <magodza06@gmail.com>
2023-02-22 22:09:28 +0200

    add-js-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onFormSabmit);
+
+function onFormSabmit(ev) {
+    ev.preventDefault(); 
+     const { email, password } = ev.currentTarget.elements;
+    if (email.value === '' || password.value === '') {
+        return alert('Всі поля повинні бути заповнені!');
+
+    } else {
+        const formData = {
+          email: email.value,
+          password: password.value,
+        };
+    
+    
+        console.log(formData);
+        ev.currentTarget.reset();
+    }
+}
