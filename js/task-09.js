2587fa4160127e220971e353f7c6a2a5bf7d2725
Serhii Yevchishen <magodza06@gmail.com>
2023-02-23 10:29:28 +0200

    add-js-task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const textBackgroundColor = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+const body = document.body;
+
+const changeColor = () => {
+  textBackgroundColor.textContent = body.style.backgroundColor =
+    getRandomHexColor();
+};
+
+
+changeColorButton.addEventListener('click', changeColor);
