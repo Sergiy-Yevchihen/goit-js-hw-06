e33edb9713df85994a056e6bc07693565e4a96cb
Serhii Yevchishen <magodza06@gmail.com>
2023-02-27 11:21:20 +0200

    add-js-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const textBackgroundColor = document.querySelector('.color');
-const changeColorButton = document.querySelector('.change-color');
-const body = document.body;
+// const textBackgroundColor = document.querySelector('.color');
+// const changeColorButton = document.querySelector('.change-color');
+// const body = document.body;
 
-const changeColor = () => {
-  textBackgroundColor.textContent = body.style.backgroundColor =
-    getRandomHexColor();
-};
+// const changeColor = () => {
+//   textBackgroundColor.textContent = body.style.backgroundColor =
+//     getRandomHexColor();
+// };
 
 
-changeColorButton.addEventListener('click', changeColor);
+// changeColorButton.addEventListener('click', changeColor);
+function changeBackgroundColor() {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.background = backgroundColor;
+  document.querySelector('.color').textContent = backgroundColor;
+}
+
+const button = document.querySelector('.change-color');
+button.addEventListener('click', changeBackgroundColor);
